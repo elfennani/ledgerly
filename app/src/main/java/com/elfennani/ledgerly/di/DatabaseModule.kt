5a08c148376968e4640b5fe5883193ff24e9f2e9
Nizar Elfennani <elfennani2002@gmail.com>
2025-08-31 22:30:09 +0100

    Enhance navigation transitions and provide database instance in DI module

--- app/src/main/java/com/elfennani/ledgerly/di/DatabaseModule.kt
+++ app/src/main/java/com/elfennani/ledgerly/di/DatabaseModule.kt
@@ -1,11 +1,22 @@
 package com.elfennani.ledgerly.di
 
+import android.content.Context
+import androidx.room.Room
+import com.elfennani.ledgerly.data.local.AppDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): AppDatabase {
+        return Room.databaseBuilder(context, AppDatabase::class.java, "ledgerly_db")
+            .build()
+    }
 }
--- app/src/main/java/com/elfennani/ledgerly/presentation/Navigation.kt
+++ app/src/main/java/com/elfennani/ledgerly/presentation/Navigation.kt
@@ -1,14 +1,23 @@
 package com.elfennani.ledgerly.presentation
 
-import android.util.Log
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.IntOffset
 import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
 import com.elfennani.ledgerly.R
 import com.elfennani.ledgerly.domain.model.Tab
 import com.elfennani.ledgerly.presentation.component.AppScaffold
@@ -18,14 +27,18 @@ import com.elfennani.ledgerly.presentation.scenes.products.ProductListRoute
 import com.elfennani.ledgerly.presentation.scenes.products.ProductListScreen
 import com.elfennani.ledgerly.presentation.scenes.transactions.TransactionListRoute
 import com.elfennani.ledgerly.presentation.scenes.transactions.TransactionListScreen
+import kotlin.math.roundToInt
 
 private const val TAG = "Navigation"
+const val ANIM_DURATION_MILLIS = 150
 
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
     val navBackStackEntry = navController.currentBackStackEntryAsState().value
     val currentDestination = navBackStackEntry?.destination
+    val spec = remember { tween<Float>(ANIM_DURATION_MILLIS, easing = FastOutSlowInEasing) }
+    val specInt = remember { tween<IntOffset>(ANIM_DURATION_MILLIS, easing = FastOutSlowInEasing) }
 
     AppScaffold(
         selected = { tab ->
@@ -66,14 +79,58 @@ fun Navigation() {
         NavHost(
             navController = navController,
             startDestination = HomeRoute,
+            enterTransition = {
+                fadeIn() + slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                    animationSpec = tween(ANIM_DURATION_MILLIS),
+                ) + scaleIn(initialScale = 0.9f)
+            },
+            exitTransition = {
+                fadeOut(animationSpec = tween(ANIM_DURATION_MILLIS)) + scaleOut(
+                    targetScale = 0.75f
+                )
+            },
+            popEnterTransition = {
+                fadeIn(animationSpec = spec) + scaleIn(
+                    initialScale = 0.9f, animationSpec = spec
+                ) + slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
+                    initialOffset = { (it * 0.15f).roundToInt() },
+                    animationSpec = specInt
+                )
+            },
+            popExitTransition = {
+                fadeOut(animationSpec = spec) + slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
+                    targetOffset = { (it * 0.15f).roundToInt() },
+                    animationSpec = specInt
+                ) + scaleOut(
+                    targetScale = 0.85f, animationSpec = spec
+                )
+            },
         ) {
-            composable<HomeRoute> {
+            composable<HomeRoute>(
+                enterTransition = { EnterTransition.None },
+                popEnterTransition = { EnterTransition.None },
+                exitTransition = { ExitTransition.None },
+                popExitTransition = { ExitTransition.None }
+            ) {
                 HomeScreen(navController = navController)
             }
-            composable<ProductListRoute> {
+            composable<ProductListRoute>(
+                enterTransition = { EnterTransition.None },
+                popEnterTransition = { EnterTransition.None },
+                exitTransition = { ExitTransition.None },
+                popExitTransition = { ExitTransition.None }
+            ) {
                 ProductListScreen(navController = navController)
             }
-            composable<TransactionListRoute> {
+            composable<TransactionListRoute>(
+                enterTransition = { EnterTransition.None },
+                popEnterTransition = { EnterTransition.None },
+                exitTransition = { ExitTransition.None },
+                popExitTransition = { ExitTransition.None }
+            ) {
                 TransactionListScreen(navController = navController)
             }
         }
